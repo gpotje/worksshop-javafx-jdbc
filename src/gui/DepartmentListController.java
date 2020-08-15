7875fd637e75a1dbf7674bd6da2e3f12b4a764f7
GPot <gpotje.souza@gmail.com>
2020-08-15 20:42:27 -0300

    Saving a new Department

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -93,6 +93,7 @@ public class DepartmentListController implements Initializable {
 			
 			DepartmentFormController controller = loader.getController();
 			controller.setDepartment(obj);
+			controller.setDepartmentservice(new DepartmentService());
 			controller.updateFormData();
 			
 			

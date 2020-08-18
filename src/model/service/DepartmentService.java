8838efe7e2d4287f8cf53261747776502a0529d6
GPot <gpotje.souza@gmail.com>
2020-08-17 21:41:52 -0300

    Remove department

--- src/model/service/DepartmentService.java
+++ src/model/service/DepartmentService.java
@@ -1,6 +1,5 @@
 package model.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -23,4 +22,8 @@ public class DepartmentService {
 		}
 	}
 	
+	public void remove(Department obj) {
+		dao.deleteById(obj.getId());
+	}
 }
+

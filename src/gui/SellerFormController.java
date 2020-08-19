63e633047f06dd27633a48f89ef5e12c9d29d2c4
GPot <gpotje.souza@gmail.com>
2020-08-18 21:16:57 -0300

    TextField & DatePicker

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -2,6 +2,8 @@ package gui;
 
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -18,6 +20,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Seller;
@@ -39,7 +42,19 @@ public class SellerFormController implements Initializable {
 	@FXML
 	private TextField txtName;
 	@FXML
+	private TextField txtEmail;
+	@FXML
+	private DatePicker dpBirthDate;
+	@FXML
+	private TextField txtBaseSalary;
+	@FXML
 	private Label labelErroName;
+	@FXML
+	private Label labelErroEmail;
+	@FXML
+	private Label labelErroBirthDate;
+	@FXML
+	private Label labelErroBaseSalary;
 	@FXML
 	private Button btSave;
 	@FXML
@@ -113,7 +128,12 @@ public class SellerFormController implements Initializable {
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
 		Constraints.setTextFieldInteger(txtId);
-		Constraints.setTextFieldMaxLength(txtName,30);
+		Constraints.setTextFieldMaxLength(txtName,70);
+		Constraints.setTextFieldDouble(txtBaseSalary);
+		Constraints.setTextFieldMaxLength(txtEmail, 60);
+		Utils.formatDatePicker(dpBirthDate, "dd/MM/yyyy");
+		
+		
 	}
 	
 	public void updateFormData() {
@@ -123,6 +143,12 @@ public class SellerFormController implements Initializable {
 		
 		txtId.setText(String.valueOf(entity.getId()));
 		txtName.setText(entity.getName());
+		txtEmail.setText(entity.getEmail());
+		txtBaseSalary.setText(String.format("%.2f", entity.getBaseSalary()));
+		if(entity.getBirthDate() != null) {
+		dpBirthDate.setValue(LocalDate.ofInstant(entity.getBirthDate().toInstant(), ZoneId.systemDefault()));
+		}
+		
 	}
 
 	
